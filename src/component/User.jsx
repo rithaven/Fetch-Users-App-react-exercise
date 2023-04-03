56e4af7bbb6a7986b8053cdbd6ad94f8af1469d1
rithaven <veneruth4@gmail.com>
2023-04-03 16:31:27 +0200

    Style user cards

--- src/component/User.jsx
+++ src/component/User.jsx
@@ -1,33 +1,31 @@
-
-
-import ReactCardFlip from 'react-card-flip';
-import { useState } from 'react';
+import ReactCardFlip from "react-card-flip";
+import { useState } from "react";
 
 const User = ({ user }) => {
-    const [isFlipped, setIsFlipped] = useState(false);
-    
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
-    <ReactCardFlip isFlipped={isFlipped}>
-    <div
-          className="p-8 bg-white rounded-lg cursor-pointer"
+    <div className="p-8 bg-white rounded-lg">
+      <ReactCardFlip isFlipped={isFlipped}>
+        <div
+          className="p-8 bg-white cursor-pointer"
           onMouseOver={() => setIsFlipped(!isFlipped)}
         >
-        <img src={user.avatar} alt="User Avatar" />
-        <h3 className="font-bold ">
-          {user.first_name} {user.last_name}
-        </h3>
-        <p>Country: {user.address.country}</p>
-      </div> 
-      <div
-          className="p-8 bg-white rounded-lg cursor-pointer w-96 h-96"
+          <img src={user.avatar} alt="User Avatar" />
+          <h3 className="font-bold ">
+            {user.first_name} {user.last_name}
+          </h3>
+          <p>Country: {user.address.country}</p>
+        </div>
+        <div
+          className="p-8 bg-white cursor-pointer w-96 h-96"
           onMouseOver={() => setIsFlipped(!isFlipped)}
         >
-          <h3 className='font-bold'>More information Here</h3>  
-     
+          <h3 className="text-2xl font-bold">More information Here</h3>
+        </div>
+      </ReactCardFlip>
     </div>
-    </ReactCardFlip>
   );
 };
 
 export default User;
-
