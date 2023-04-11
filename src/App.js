bebbbfc2ef40aa2b562b5f7e1aa576adb64cf7ef
rithaven <veneruth4@gmail.com>
2023-04-11 11:11:47 +0200

    Fixed page reload and Button name

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import './index.css';
 
 function App() {
   const [users, setUsers] = useState([]);
-
-	useEffect(() => {
-		fetch('https://random-data-api.com/api/users/random_user?size=10')
+ 
+  const fetchUsers = () =>{
+	fetch('https://random-data-api.com/api/users/random_user?size=10')
 			.then(response => response.json())
 			.then(data => setUsers(data));
+  }
+	useEffect(() => {
+		fetchUsers();
 	}, []);
 
 	return (
 	<div className='flex flex-col items-center justify-center bg-[#e1f6dc] h-full'>
-    	<button onClick={() => window.location.reload()} className="mt-8 bg-[#f0003c] rounded-lg px-16 py-2 text-2xl text-white">Reload Users</button>
+    	<button onClick={() => fetchUsers()} className="mt-8 bg-[#f0003c] rounded-lg px-16 py-2 text-2xl text-white">Fetch random</button>
       <div className='grid grid-cols-2 gap-4 p-8 '>
 			{users.map(user => <User key={user.id} user={user} />)}
 		</div>
